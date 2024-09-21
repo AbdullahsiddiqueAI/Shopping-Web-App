9e77c059c0c391dd9f8f977c9492c4a24c103940
AbdullahsiddiqueAI <Abdullahsiddiqueai9990@gmail.com>
2024-09-22 01:13:02 +0500

    added the .env api backend url

--- Client/src/util/axios.js
+++ Client/src/util/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_URL = "http://127.0.0.1:8000/api/";
+export const API_URL = import.meta.env.VITE_BACKEND_END_POINT;
 
 const instance = axios.create({
   baseURL: API_URL,
