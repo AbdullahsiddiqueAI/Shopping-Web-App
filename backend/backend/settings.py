e5c690eddaea5b16b7dab25cc840feae04623d82
amariumms <amariumms@gmail.com>
2025-01-10 14:55:43 +0500

    remove keys

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,26 +102,26 @@ ASGI_APPLICATION = 'backend.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-if DEBUG_for_db:
-    print("Using SQLite for development", DEBUG_for_db)
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+
+print("Using SQLite for development", DEBUG_for_db)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    print("Using MySQL for production", DEBUG_for_db)
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": 'shoppingWebApp',
-            "USER": 'root',
-            "PASSWORD": 'root',
-            "HOST": 'localhost',
-            "PORT": '3306',
-        }
-    }
+}
+# else:
+#     print("Using MySQL for production", DEBUG_for_db)
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.mysql",
+#             "NAME": 'shoppingWebApp',
+#             "USER": 'root',
+#             "PASSWORD": 'root',
+#             "HOST": 'localhost',
+#             "PORT": '3306',
+#         }
+#     }
 
 
 
